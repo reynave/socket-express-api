c159ff11eb6ba2ea8780951e5379a45770e9f7c9
reynave <felix.renaldi@gmail.com>
2023-08-05 13:05:30 +0700

    add socket

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo! masuk 2')
-})
-app.listen(process.env.PORT || 3000)
+require('dotenv').config();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || process.env.PORT_LOCAL;
+
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo! masuk 2')
+// })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+
+http.listen(port, () => { 
+    console.log(process.env)
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
